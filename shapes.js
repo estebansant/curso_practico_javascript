836dfba5c99972e772244411e2004f34c0fd7d22
estebansant <estebansant30@gmail.com>
2021-08-07 18:05:51 -0400

    optimized the code and made it shorter

--- shapes.js
+++ shapes.js
@@ -1,44 +1,22 @@
 //Code for a Square
 
-console.group("Squares");
-const squareLateral = 5;
-console.log("The borders of the square have a length of: " + squareLateral + "cm"); 
+const squarePerimeter = (side) => side * 4;
 
-const squarePerimeter = squareLateral * 4;
-console.log("The perimeter of the square is of: " + squarePerimeter + "cm");
-
-const squareArea = squareLateral * squareLateral;
-console.log("The area of the square is: " + squareArea + "cm^2");
-console.groupEnd();
+const squareArea = (side) => side ** 2;
 
 //Code for a Triangle
 
-console.group("Triangles");
-const triangleSide1 = 6;
-const triangleSide2 = 6;
-const triangleBase = 4;
-const triangleHeight = 5.5;
-console.log("The sides of the triangle have a length of: " + triangleSide1 + "cm, " + triangleSide2 + "cm" + " and the base has a lenght of: " + triangleBase + "cm"); 
+const trianglePerimeter = (side1, side2, base) => side1 + side2 + base;
 
-const trianglePerimeter = triangleSide1 + triangleSide2 + triangleBase;
-console.log("The perimeter of the triangle is of: " + trianglePerimeter + "cm");
-
-const triangleArea = (triangleBase * triangleHeight) / 2;
-console.log("The area of the triangle is: " + triangleArea + "cm^2");
-console.groupEnd();
+const triangleArea = (base, height) => (base * height) / 2;
 
 //Code for a Circle
 
-console.group("Circles");
-const circleRadio = 4;
-const circleDiameter = circleRadio * 2;
+const circleDiameter = (radius) => radius * 2;
 const PI = Math.PI;
 
 //Perimeter
-const circlePerimeter = circleDiameter * PI;
-console.log("The perimeter of the circle is: " + circlePerimeter + "cm");
+const circlePerimeter = (radius) => circleDiameter(radius) * PI;
 
 //Area
-const circleArea = (circleRadio * circleRadio) * PI;
-console.log("The area of the circle is: " + circleArea + "cm^2");
-console.groupEnd();
+const circleArea = (radius) => (radius ** 2) * PI;
