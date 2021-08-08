643b689daa5220b4892d28cba2499d62dc7e9b18
estebansant <estebansant30@gmail.com>
2021-08-07 21:06:57 -0400

    Added interaction with HTML

--- shapes.js
+++ shapes.js
@@ -14,9 +14,64 @@ const triangleArea = (base, height) => (base * height) / 2;
 
 const circleDiameter = (radius) => radius * 2;
 const PI = Math.PI;
-
-//Perimeter
 const circlePerimeter = (radius) => circleDiameter(radius) * PI;
-
-//Area
 const circleArea = (radius) => (radius ** 2) * PI;
+
+
+//Interaction with HTML
+//Square
+
+function calculateSquarePerimeter(){
+    const input = document.getElementById("squareInput");
+    const value = input.value;
+    const perimeter = squarePerimeter(value);
+    let squareResult = document.getElementById("squareResult");
+    squareResult.innerHTML=("The perimeter of the square is: " + perimeter + "cm");
+}
+function calculateSquareArea(){
+    const input = document.getElementById("squareInput");
+    const value = input.value;
+    const area = squareArea(value);
+    let squareResult = document.getElementById("squareResult");
+    squareResult.innerHTML=("The area of the square is: " + area + "cm^2");
+}
+
+//Triangle
+
+function calculateTrianglePerimeter(){
+    const side1 = document.getElementById("triangleSide1");
+    const side1Value = parseFloat(side1.value);
+    const side2 = document.getElementById("triangleSide2");
+    const side2Value = parseFloat(side2.value);
+    const base = document.getElementById("base");
+    const baseValue = parseFloat(base.value);
+    const perimeter = trianglePerimeter(side1Value, side2Value, baseValue);
+    let triangleResult = document.getElementById("triangleResult");
+    triangleResult.innerHTML=("The perimeter of the triangle is: " + perimeter + "cm");
+}
+function calculateTriangleArea(){
+    const height = document.getElementById("height");
+    const heightValue = parseFloat(height.value);
+    const base = document.getElementById("base");
+    const baseValue = parseFloat(base.value);
+    const area = triangleArea(baseValue, heightValue);
+    let triangleResult = document.getElementById("triangleResult");
+    triangleResult.innerHTML=("The area of the triangle is: " + area + "cm^2");
+}
+
+//Circle
+
+function calculateCirclePerimeter(){
+    const input = document.getElementById("circleInput");
+    const value = input.value;
+    const perimeter = circlePerimeter(value);
+    let circleResult = document.getElementById("circleResult");
+    circleResult.innerHTML=("The perimeter of the circle is: " + perimeter + "cm");
+}
+function calculateCircleArea(){
+    const input = document.getElementById("circleInput");
+    const value = input.value;
+    const area = circleArea(value);
+    let circleResult = document.getElementById("circleResult");
+    circleResult.innerHTML=("The area of the circle is: " + area + "cm^2");
+}
