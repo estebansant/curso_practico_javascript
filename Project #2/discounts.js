4b0a15114be54b6ebe9ad387b40a75c7b16a05b3
estebansant <estebansant30@gmail.com>
2021-08-08 08:11:52 -0400

    created the function to calculate a discount

---
+++ Project #2/discounts.js
@@ -0,0 +1,7 @@
+// const originalPrice = 100;
+// const discount = 15;
+
+const calculatePriceWithDiscount = (price, discount) => price - (price * (discount/100));
+
+
+// console.log({originalPrice, discount, priceWithDiscount});
